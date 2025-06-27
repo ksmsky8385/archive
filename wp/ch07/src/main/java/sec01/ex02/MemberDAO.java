539e440e9b6bf6b2a5d335c77021d82be48335b9
ksmsky8385 <ksmsky8385@naver.com>
2025-06-27 15:12:32 +0900

    0626

--- wp/ch07/src/main/java/sec01/ex02/MemberDAO.java
+++ wp/ch07/src/main/java/sec01/ex02/MemberDAO.java
@@ -1,10 +1,10 @@
-package sec01.ex01;
+package sec01.ex02;
 
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +13,19 @@ public class MemberDAO {
 	private static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	private static final String user = "scott";
 	private static final String pwd = "tiger";
-	private Connection con;
-	private Statement stmt;
 	
-	public List<MemberVO> listMembers() {
-		List<MemberVO> list = new ArrayList<MemberVO>();
+	private Connection con;
+	private PreparedStatement pstmt;
+	
+	public List listMembers() {
+		List list = new ArrayList();
 		try {
 			connDB();
 			String query = "select * from t_member";
-			System.out.println(query);
-			ResultSet rs = stmt.executeQuery(query);
+			System.out.println("ch07.sec01.ex02");
+			System.out.println("preparedStatement : " + query);
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				String id = rs.getString("id");
 				String pwd = rs.getString("pwd");
@@ -38,7 +41,7 @@ public class MemberDAO {
 				list.add(vo);
 			}
 			rs.close();
-			stmt.close();
+			pstmt.close();
 			con.close();
 		}
 		catch (Exception e) {
@@ -53,8 +56,6 @@ public class MemberDAO {
 			System.out.println("Oracle 드라이버 로딩 성공");
 			con = DriverManager.getConnection(url, user, pwd);
 			System.out.println("Connection 생성 성공");
-			stmt = con.createStatement();
-			System.out.println("Statement 생성 성공");
 			}
 		catch (Exception e) {
 			e.printStackTrace();
