601f0e378df28eab05c4e892e1f30e0f10ebc610
ksmsky8385 <ksmsky8385@naver.com>
2025-06-30 21:00:48 +0900

    0630

---
+++ wp/ch08/src/main/java/sec04/ex02/FirstServlet.java
@@ -0,0 +1,24 @@
+package sec04.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first05_1")
+public class FirstServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		request.setAttribute("address", "서울시 성북구");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("second05_1");
+		dispatcher.forward(request, response);
+	}
+
+}
